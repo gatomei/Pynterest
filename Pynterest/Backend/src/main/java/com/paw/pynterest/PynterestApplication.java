e3b8d5c55fded24c9fe590ff320ec4334e68dad9
gatomei <atomeigeorgiana@yahoo.com>
2020-10-09 19:44:01 +0300

    SpringBoot project structure

---
+++ Pynterest/Backend/src/main/java/com/paw/pynterest/PynterestApplication.java
@@ -0,0 +1,13 @@
+package com.paw.pynterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PynterestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PynterestApplication.class, args);
+    }
+
+}
---
+++ Pynterest/Backend/src/test/java/com/paw/pynterest/PynterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paw.pynterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PynterestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
