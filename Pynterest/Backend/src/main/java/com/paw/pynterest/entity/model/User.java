d11cfd2294298a0d34e534d9cba9189c427aa802
gatomei <atomeigeorgiana@yahoo.com>
2020-10-28 10:32:28 +0200

    Modified user entity class

--- Pynterest/Backend/src/main/java/com/paw/pynterest/entity/model/User.java
+++ Pynterest/Backend/src/main/java/com/paw/pynterest/entity/model/User.java
@@ -1,6 +1,5 @@
 package com.paw.pynterest.entity.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -10,7 +9,7 @@ import javax.validation.constraints.Past;
 import java.util.Date;
 
 @Data
-@Entity
+@Entity(name="Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,23 +17,28 @@ public class User {
 
     @NotNull
     @Email(message = "Email should be valid")
+    @Column(unique = true, length = 100)
     private String email;
 
     @NotNull
+    @Column(length = 100, unique = true)
     private String username;
 
     @NotNull
+    @Column(length = 100)
     private String fullname;
 
     @NotNull
+    @Column(length = 50)
     private String password;
 
     @NotNull
     private boolean admin;
 
     @Past(message = "Birthdate should be in the past")
-    @JsonFormat(pattern="dd-mm-yyyy")
+    @Temporal(TemporalType.DATE)
     private Date birthDate;
 
+    @Column(length = 500)
     private String description;
 }
