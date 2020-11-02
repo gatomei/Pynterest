ba74db62e3e01d99411cbef9771d95b54d1b3527
andreistefan98 <andreistefan976@gmail.com>
2020-11-02 11:43:34 +0200

    update formular de creare cont

--- Pynterest/Frontend/pynterest-app/src/app/login/login.component.ts
+++ Pynterest/Frontend/pynterest-app/src/app/login/login.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
+enum formType{
+  login,
+  createNewAccount,
+  resetPassword
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +16,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) { }
 
-  createNewAccount = false;
+  formOpened = formType.login;
 
   loginForm: FormGroup = this.formBuilder.group({
     email: [,{validators: [Validators.required, Validators.email], updateOn: "change",}],
@@ -22,8 +28,14 @@ export class LoginComponent implements OnInit {
     fullname: [, { validators: [Validators.required], updateOn: "change" }],
     email: [,{validators: [Validators.required, Validators.email], updateOn: "change",}],
     password: [, { validators: [Validators.required], updateOn: "change" }],
-    description: [, { updateOn: "change" }]
+    description: [, { updateOn: "change" }],
+    datepicker: [, { updateOn: "change" }],
+    photo: [, { updateOn: "change" }]
   });
+
+  resetPasswordForm: FormGroup = this.formBuilder.group({
+    email: [,{validators: [Validators.required, Validators.email], updateOn: "change",}]
+  })
 
   ngOnInit(): void {
   }
@@ -39,19 +51,25 @@ export class LoginComponent implements OnInit {
     console.log("Email: " + this.registerForm.get("email").value);
     console.log("Password: " + this.registerForm.get("password").value);
     console.log("Description: " + this.registerForm.get("description").value);
+    console.log("Date: " + this.registerForm.get("datepicker").value);
+    console.log("Photo: " + this.registerForm.get("photo").value);
+  }
+
+  submitPasswordForm(){
+    console.log("Email: " + this.resetPasswordForm.get("email").value);
   }
 
   resetPassword(){
-    console.log("Ati apasat pe butonul de resetare a parolei")
+    this.formOpened = formType.resetPassword
   }
 
   changeRegisterForm(){
-    this.createNewAccount = true;
+    this.formOpened = formType.createNewAccount
 
   }
 
   changeLoginForm(){
-    this.createNewAccount = false;
+    this.formOpened = formType.login;
   }
 
 
