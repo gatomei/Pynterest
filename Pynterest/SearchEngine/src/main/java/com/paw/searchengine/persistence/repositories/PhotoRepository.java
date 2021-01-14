1ab2889640d41ef204a9b1e0f5646d225b055c60
gatomei <atomeigeorgiana@yahoo.com>
2021-01-14 16:31:05 +0200

    query method for search-not tested

--- Pynterest/SearchEngine/src/main/java/com/paw/searchengine/persistence/repositories/PhotoRepository.java
+++ Pynterest/SearchEngine/src/main/java/com/paw/searchengine/persistence/repositories/PhotoRepository.java
@@ -3,5 +3,8 @@ package com.paw.searchengine.persistence.repositories;
 import com.paw.searchengine.persistence.documents.Photo;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface PhotoRepository extends ElasticsearchRepository<Photo, Long> {
+    List<Photo> findAllByTitleContainingOrDescriptionContaining(String title,String description);
 }
